b7b9ab25aaf7485b24c93cb4cec707cea65b9811
Kazej <jacob.dahl96@gmail.com>
2022-07-21 13:30:42 -0500

    ff2331

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ const server = app.listen(PORT, () => {
 
 const io = socketIO(server, {
   cors: {
-    origin: ["http://secure-shelf-39110.herokuapp.com/", "http://kazej.net/c.html"]
+    origin: ["http://secure-shelf-39110.herokuapp.com/", "http://kazej.net/c.html"],
+    methods: ["GET", "POST"],
   }
 });
 
